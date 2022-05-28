db912a7bb0746780f86f967cf19cfeacd21154de
akchabekova <Luelum7@gmail.com>
2022-05-28 19:51:27 +0200

    finished

--- src/pages/MealInfo/MealInfo.js
+++ src/pages/MealInfo/MealInfo.js
@@ -49,7 +49,7 @@ const MealInfo = () => {
                 <p className="line-info"></p>
                 <h3 className="instruction-info">Instruction</h3>
                 <div className="instruction-desc">{meal.strInstructions}</div>
-                <ReactPlayer  url={meal.strYoutube} />
+                {/*<ReactPlayer  url={meal.strYoutube} />*/}
         </div>
         </div>
     );
